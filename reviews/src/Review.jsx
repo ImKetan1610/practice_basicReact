9e565b876fa1e18f28b0a0dd73dd4bf312d05abe
ImKetan1610 <ketan.makode@gmail.com>
2023-01-19 19:24:19 +0530

    reviews

---
+++ reviews/src/Review.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import People from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = People[index];
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+    </article>
+  );
+};
+
+export default Review;
