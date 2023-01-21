2aa9c2c6555634a1ef8f6edce42d532433baadad
ImKetan1610 <ketan.makode@gmail.com>
2023-01-21 21:18:50 +0530

    accordion

--- accordion/src/data.js
+++ accordion/src/data.js
@@ -2,27 +2,27 @@ const questions = [
   {
     id: 1,
     title: "Do I have to allow the use of cookies?",
-    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+    info: "As a general rule, most websites require the use of cookies to some extent in order to function properly, and some cookies may be necessary for the website to provide you with the service or information that you are seeking. However, in most cases, you have the option to control the use of cookies and can choose to allow or block them. The use of cookies can improve your experience on the website, as they can help the website remember your preferences, remember items in your shopping cart, or help you log in more easily. However, some cookies can also be used to track your online behavior and may be used for advertising or other purposes. To ensure that you have control over the use of cookies, many websites now include a cookie banner or cookie notice that allows you to choose whether or not to allow cookies. You can typically choose to allow all cookies, block all cookies, or choose which types of cookies to allow. In general, it is a good idea to review the website's privacy policy and cookie policy to understand how the website uses cookies and what information is collected. This can help you make an informed decision about whether or not to allow the use of cookies on that particular website.",
   },
   {
     id: 2,
     title: "How do I change my My Page password?",
-    info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+    info: "The steps to change your password for My Page may vary depending on the specific website or platform you are using. However, in general, the process typically involves the following steps: 1. Log in to your My Page account using your current password. 2. Locate the `Settings` or `Account Settings` section of your My Page account. This is often found by clicking on your username or profile picture, or by looking for a gear or settings icon. Look for the `Change Password`` or `Password Settings` option within the account settings. Click on this option to access the password change form. In the password change form, enter your current password and then enter your new password. Make sure that your new password is strong and unique to help keep your account secure. Confirm your new password by entering it again in the confirmation field. Click the `Save` or `Update` button to save your new password. Log out of your account and log back in using your new password to ensure that it has been successfully changed. If you're having trouble finding the password change option or have forgotten your current password, you may need to contact the website's support team for assistance.",
   },
   {
     id: 3,
     title: "What is BankID?",
-    info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+    info: "BankID is an electronic identification solution used in Sweden, Norway, and several other European countries. It is a form of digital ID that allows individuals to securely and conveniently authenticate their identity online, for example, to log in to their bank account, sign legal documents, or confirm their identity to government authorities.",
   },
   {
     id: 4,
     title: "Whose birth number can I use?",
-    info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+    info: "It is generally not legal or ethical to use someone else's birth number or other personal identification information without their consent. In many countries, such as the United States, using someone else's personal identification information without their consent is a criminal offense and can result in serious legal consequences. Your own birth number or personal identification number is unique to you and should be used only for legitimate purposes, such as identity verification or tax purposes. You should never use someone else's birth number or personal identification number, even if you have their permission, unless you are authorized to do so by a legal or regulatory authority.",
   },
   {
     id: 5,
     title: "When do I recieve a password ordered by letter?",
-    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+    info: "I believe you may be referring to a bank-issued password or access code that is sent to you via postal mail. In some cases, when you open a new bank account or apply for a bank's online banking service, the bank may send you a letter containing a temporary password or access code that you can use to log in to your account for the first time. The exact timing of when you receive this letter will depend on the bank's policies and procedures. Some banks may send the letter as soon as your account is approved or activated, while others may wait a few days to allow for processing and verification.",
   },
 ];
 export default questions;
