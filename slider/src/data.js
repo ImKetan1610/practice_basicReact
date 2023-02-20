ea65d08d9aebcace5d73a857a2c84baa418081b5
ImKetan1610 <ketan.makode@gmail.com>
2023-02-20 23:44:01 +0530

    slider

---
+++ slider/src/data.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    name: "Hashmat Wani",
+    title: "web developer",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQHu07Qcfmgl5g/profile-displayphoto-shrink_200_200/0/1665770858102?e=1681344000&v=beta&t=H0Xzmtb79e_aFqBMlGu8yJDBNOt_odGTUMOHQSF9w38",
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    id: 2,
+    name: "Mayur Wagh",
+    title: "web designer",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQEOcnmRI9weGQ/profile-displayphoto-shrink_800_800/0/1661709663958?e=2147483647&v=beta&t=Up0TZuatguyL4FNDSP4WT6MLwSKhYZvmxGf7pGqj3U4",
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    id: 3,
+    name: "Shaheen Ahmed",
+    title: "Instructional Associate",
+    image:
+      "https://masai-website-images.s3.ap-south-1.amazonaws.com/Shaheen_Ahmad_1_bc591add92.png",
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    id: 4,
+    name: "Ketan Makode",
+    title: "SDE",
+    image:
+      "https://media.licdn.com/dms/image/C4D03AQFANs6amLA3_Q/profile-displayphoto-shrink_200_200/0/1654161725400?e=2147483647&v=beta&t=WD_9nMYcTnYkdJd0KPHud1wAfwtRlkD-GhWAyYRCRew",
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
+
+export default people;
